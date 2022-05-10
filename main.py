ca010f17830040e57846a5a7bd4f5870c1be4c03
DenisKabanov <dskabanov@edu.hse.ru>
2022-05-10 15:58:37 +0300

    work with database

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-import eel
-
-#TODO add output info
-def save_common_info(last_name, first_name, dad_name, dob, doi):
-    return last_name, first_name, dad_name, dob, doi
-
-
-@eel.expose
-def convert_value_py(last_name, first_name, dad_name, dob, doi):
-    return save_common_info(last_name, first_name, dad_name, dob, doi)
-
+from apps import *
 
 if __name__ == '__main__':
     eel.init("web")
     eel.start("web_main.html", size=(900, 800))
-
-
-
