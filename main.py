630cb95c5e86cf35a9ce5156554fb6682da6e5d5
DenisKabanov <dskabanov@edu.hse.ru>
2022-06-02 19:57:48 +0300

    resolution

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ PROJ_ROOT = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
 
 if __name__ == '__main__':
     eel.init(f"{PROJ_ROOT}/web")
-    eel.start("web_main.html", size=(1200, 800))
+    eel.start("web_main.html", size=(1300, 1080))
